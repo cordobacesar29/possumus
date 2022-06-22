eee262d2a3d36037cb7d1e9bf7fa7e69cb61d4ca
César Córdoba <cordobacesar29@gmail.com>
2022-06-22 19:19:26 -0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Home } from "./pages/home/Home";
+import { Login } from "./pages/login/Login";
+import { Header } from "./components/Header";
+import {useUser} from "./context/UserContext";
 
-function App() {
+function App() {  
+  const {user, logOut} = useUser();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Header logOut={logOut} user={user}/>
+        <Routes>
+          <Route path="" element={user ? <Home /> : <Login />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
